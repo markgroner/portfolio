2c9a0cd6dea0ba1151dd6b934c19abcc4f64c4d9
Mark Groner <markgroner@utexas.edu>
2018-09-04 20:56:33 -0500

    made plot into a function for barchart

--- static/vendor/js/lineupBarCharts.js
+++ static/vendor/js/lineupBarCharts.js
@@ -1,12 +1,122 @@
+
+
+
+function plotNbaGroupedBar(error, data) {
+  if (error) throw error;
+
+  var chartTitle = 'Scoring';
+  var barGraphDiv = d3.select('#scoring_graph');
+  var svgHeight = barGraphDiv.node().getBoundingClientRect().height;
+  var svgWidth = barGraphDiv.node().getBoundingClientRect().width;
+
+  var svg = barGraphDiv.append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth)
+
+  var chartMargins = {top: svgHeight*.1, right: svgWidth*.1, bottom: svgHeight*.1, left: svgWidth*.1};
+  var chartWidth = +svg.attr("width") - chartMargins.left - chartMargins.right;
+  var chartHeight = +svg.attr("height") - chartMargins.top - chartMargins.bottom;
+
+  var g = svg.append("g").attr("transform", "translate(" + chartMargins.left + "," + chartMargins.top + ")");
+
+  var x0 = d3.scaleBand()
+      .rangeRound([0, chartWidth])
+      .paddingInner(0.1);
+
+  var x1 = d3.scaleBand()
+      .padding(0.05);
+
+  var y = d3.scaleLinear()
+      .rangeRound([chartHeight, 0]);
+
+  var z = d3.scaleOrdinal()
+      .range(['#0046AD', '#D6D6C9', '#D0103A']);
+
+      if (error) throw error;
+
+      var keys = data.columns.slice(1);
+
+      x0.domain(data.map(function(d) { return d.State; }));
+      x1.domain(keys).rangeRound([0, x0.bandwidth()]);
+      y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
+
+      g.append("g")
+        .selectAll("g")
+        .data(data)
+        .enter().append("g")
+          .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; })
+        .selectAll("rect")
+        .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
+        .enter().append("rect")
+          .attr("x", function(d) { return x1(d.key); })
+          .attr("y", function(d) { return y(d.value); })
+          .attr("width", x1.bandwidth())
+          .attr("height", function(d) { return chartHeight - y(d.value); })
+          .attr("fill", function(d) { return z(d.key); });
+
+      g.append("g")
+          .attr("class", "axis")
+          .attr("transform", "translate(0," + chartHeight + ")")
+          .call(d3.axisBottom(x0));
+
+      g.append("g")
+          .attr("class", "axis")
+          .call(d3.axisLeft(y).ticks(null, "s"))
+        .append("text")
+          .attr("x", 2)
+          .attr("y", y(y.ticks().pop()) + 0.5)
+          .attr("dy", "0.32em")
+          .attr("fill", "#000")
+          .attr("font-weight", "bold")
+          .attr("text-anchor", "start")
+          .text(chartTitle);
+
+      var legend = g.append("g")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", 10)
+          .attr("text-anchor", "end")
+        .selectAll("g")
+        .data(keys.slice().reverse())
+        .enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+      legend.append("rect")
+          .attr("x", chartWidth - 19)
+          .attr("width", 19)
+          .attr("height", 19)
+          .attr("fill", z);
+
+      legend.append("text")
+          .attr("x", chartWidth - 24)
+          .attr("y", 9.5)
+          .attr("dy", "0.32em")
+          .text(function(d) { return d; });
+};
+
+/* SCORING GRAPH */
+
+
+d3.csv("../static/nba-grouped-bar-data.csv", function(error, data) {
+  plotNbaGroupedBar(error, data)
+});
+
+
+
+
+
+/*
+
+
+
 // Dataset we will be using to set the height of our rectangles
 var booksReadThisYear = [17, 23, 20, 34];
-var svgHeight = 400;
-var svgWeight = 400;
+var barGraphDiv = d3.select('#ratings_graph');
+var svgHeight = barGraphDiv.node().getBoundingClientRect().height;
+var svgWidth = barGraphDiv.node().getBoundingClientRect().width;
 // Append an SVG wrapper to the page and set a variable equal to a reference to it
-var svg = d3.select("#ratings_graph")
-  .append("svg")
+var svg = barGraphDiv.append("svg")
     .attr("height", svgHeight)
-    .attr("width", svgWeight);
+    .attr("width", svgWidth);
 
 var svgGroup = svg.append("g")
   .attr("transform", "translate(50, 100)");
@@ -31,11 +141,12 @@ svgGroup.selectAll("rect")
   });
 
 
-  /* SHOOTING GRAPH8 */
+  SHOOTING GRAPH8
 
     // Define SVG area dimensions
-    var svgWidth = 960;
-    var svgHeight = 500;
+    var barGraphDiv = d3.select('#shooting_graph');
+    var svgHeight = barGraphDiv.node().getBoundingClientRect().height;
+    var svgWidth = barGraphDiv.node().getBoundingClientRect().width;
 
     // Define the chart's margins as an object
     var chartMargin = {
@@ -50,8 +161,7 @@ svgGroup.selectAll("rect")
     var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
     // Select body, append SVG area to it, and set the dimensions
-    var svg = d3.select("#shooting_graph")
-      .append("svg")
+    var svg = barGraphDiv.append("svg")
       .attr("height", svgHeight)
       .attr("width", svgWidth)
 
@@ -121,102 +231,9 @@ svgGroup.selectAll("rect")
     });
 
 
-/* SCORING GRAPH */
-
-var svgHeight = 500;
-var svgWeight = 960;
-
-var svg = d3.select("#scoring_graph_2")
-    .append("svg")
-      .attr("height", svgHeight)
-      .attr("width", svgWeight),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var x0 = d3.scaleBand()
-    .rangeRound([0, width])
-    .paddingInner(0.1);
-
-var x1 = d3.scaleBand()
-    .padding(0.05);
-
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
-
-var z = d3.scaleOrdinal()
-    .range(['#0046AD', '#D6D6C9', '#D0103A']);
-
-d3.csv("../static/nba-grouped-bar-data.csv", function(d, i, columns) {
-  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
-  return d;
-}, function(error, data) {
-  if (error) throw error;
-
-  var keys = data.columns.slice(1);
-
-  x0.domain(data.map(function(d) { return d.State; }));
-  x1.domain(keys).rangeRound([0, x0.bandwidth()]);
-  y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
-
-  g.append("g")
-    .selectAll("g")
-    .data(data)
-    .enter().append("g")
-      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; })
-    .selectAll("rect")
-    .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
-    .enter().append("rect")
-      .attr("x", function(d) { return x1(d.key); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("width", x1.bandwidth())
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("fill", function(d) { return z(d.key); });
-
-  g.append("g")
-      .attr("class", "axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x0));
-
-  g.append("g")
-      .attr("class", "axis")
-      .call(d3.axisLeft(y).ticks(null, "s"))
-    .append("text")
-      .attr("x", 2)
-      .attr("y", y(y.ticks().pop()) + 0.5)
-      .attr("dy", "0.32em")
-      .attr("fill", "#000")
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "start")
-      .text("Population");
-
-  var legend = g.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("text-anchor", "end")
-    .selectAll("g")
-    .data(keys.slice().reverse())
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  legend.append("rect")
-      .attr("x", width - 19)
-      .attr("width", 19)
-      .attr("height", 19)
-      .attr("fill", z);
-
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9.5)
-      .attr("dy", "0.32em")
-      .text(function(d) { return d; });
-  console.log('made it here')
-});
 
 
 
-/*
 d3.select(window).on("resize", handleResize);
 
 // When the browser loads, loadChart() is called
