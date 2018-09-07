221088562cf56eeaa8d158a1302448ce390446cc
Mark Groner <markgroner@utexas.edu>
2018-09-06 21:07:44 -0500

    moved legend to bottom and othrer format changes

--- static/vendor/js/lineupBarCharts.js
+++ static/vendor/js/lineupBarCharts.js
@@ -4,20 +4,21 @@ function plotNbaGroupedBar(error, data, graphDivId) {
 
   if (error) throw error;
 
-  var chartTitle = 'Scoring';
+  var chartTitle = "Scoring";
   var barGraphDiv = d3.select(graphDivId);
   var svgHeight = barGraphDiv.node().getBoundingClientRect().height;
   var svgWidth = barGraphDiv.node().getBoundingClientRect().width;
 
-  var svg = barGraphDiv.append('svg')
-        .attr('height', svgHeight)
-        .attr('width', svgWidth)
+  var svg = barGraphDiv.append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth)
 
-  var chartMargins = {top: svgHeight*.1, right: svgWidth*.1, bottom: svgHeight*.1, left: svgWidth*.1};
-  var chartWidth = +svg.attr('width') - chartMargins.left - chartMargins.right;
-  var chartHeight = +svg.attr('height') - chartMargins.top - chartMargins.bottom;
+  var chartMargins = {top: svgHeight*.1, right: svgWidth*.1, bottom: svgHeight*.2, left: svgWidth*.1};
+  var chartWidth = +svg.attr("width") - chartMargins.left - chartMargins.right;
+  var chartHeight = +svg.attr("height") - chartMargins.top - chartMargins.bottom;
 
-  var g = svg.append('g').attr('transform', 'translate(' + chartMargins.left + ',' + chartMargins.top + ')');
+
+  var g = svg.append("g").attr("transform", "translate(" + chartMargins.left + "," + chartMargins.top + ")");
   var teamAbbreviations = data.team_abbreviations;
   var statKeys = data.stat_keys;
   var graphData = data.graphData;
@@ -39,66 +40,119 @@ function plotNbaGroupedBar(error, data, graphDivId) {
       .rangeRound([chartHeight, 0])
       .domain([0, d3.max(graphData, function(d) { return d3.max(statKeys, function(key) { return d[key]; }); })]).nice();
 
-  var z = d3.scaleOrdinal()
-      .range(['#0046AD', '#D6D6C9', '#D0103A']);
+  var colors = d3.scaleOrdinal()
+      .range(["#0046AD", "#D6D6C9", "#D0103A"]);
 
 
 
-  g.append('g')
-    .selectAll('g')
+  g.append("g")
+    .selectAll("g")
     .data(graphData)
-    .enter().append('g')
-      .attr('transform', function(d) { return 'translate(' + x0(d.teamAbbreviation) + ',0)'; })
-    .selectAll('rect')
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + x0(d.teamAbbreviation) + ",0)"; })
+    .selectAll("rect")
     .data(function(d) { return statKeys.map(function(key) { return {key: key, value: d[key]}; }); })
-    .enter().append('rect')
-      .attr('x', function(d) { return x1(d.key); })
-      .attr('y', function(d) { return y(d.value); })
-      .attr('width', x1.bandwidth())
-      .attr('height', function(d) { return chartHeight - y(d.value); })
-      .attr('fill', function(d) { return z(d.key); });
+    .enter().append("rect")
+      .attr("x", function(d) { return x1(d.key); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x1.bandwidth())
+      .attr("height", function(d) { return chartHeight - y(d.value); })
+      .attr("fill", function(d) { return colors(d.key); });
 
-  g.append('g')
-      .attr('class', 'axis')
-      .attr('transform', 'translate(0,' + chartHeight + ')')
+  g.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + chartHeight + ")")
       .call(d3.axisBottom(x0));
 
 
-  g.append('g')
-      .attr('class', 'axis')
-      .call(d3.axisLeft(y).ticks(null, 's'))
-    .append('text')
-      .attr('x', 2)
-      .attr('y', y(y.ticks().pop()) + 0.5)
-      .attr('dy', '0.32em')
-      .attr('fill', '#000')
-      .attr('font-weight', 'bold')
-      .attr('text-anchor', 'start')
+  g.append("g")
+      .attr("class", "axis")
+      .call(d3.axisLeft(y).ticks(5, "p"))
+    .append("text")
+      .attr("x", 8)
+      .attr("y", y(y.ticks().pop()) + 0.5)
+      .attr("dy", "0.32em")
+      .attr("fill", "#000")
+      .attr("font-weight", "bold")
+      .attr("font-size", "14")
+      .attr("text-anchor", "start")
       .text(chartTitle);
 
-
-  var legend = g.append('g')
-      .attr('font-family', 'sans-serif')
-      .attr('font-size', 10)
-      .attr('text-anchor', 'end')
-    .selectAll('g')
-    .data(statKeys.slice().reverse())
-    .enter().append('g')
-      .attr('transform', function(d, i) { return 'translate(0,' + i * 20 + ')'; });
-
-
-  legend.append('rect')
-      .attr('x', chartWidth - 19)
-      .attr('width', 19)
-      .attr('height', 19)
-      .attr('fill', z);
+/*
+  var legend = g.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(statKeys.slice())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
 
-  legend.append('text')
-      .attr('x', chartWidth - 24)
-      .attr('y', 9.5)
-      .attr('dy', '0.32em')
+
+  legend.append("rect")
+      .attr("x", chartWidth - 19)
+      .attr("width", 19)
+      .attr("height", 19)
+      .attr("fill", colors);
+
+
+  legend.append("text")
+      .attr("x", chartWidth - 24)
+      .attr("y", 9.5)
+      .attr("dy", "0.32em")
       .text(function(d) { return d; });
+*/
+ var width = 500;
+ var height = 75;
+ var svgw = 20;
+ var svgh = 20;
+ var dataL = 0;
+ var offset = 80;
+ var legendColorBoxSize = 15;
+ var legendPaddingY = chartMargins.bottom - chartMargins.top;
+ var legendPaddingX = svgWidth*.25;
+
+  var legend = svg.append("g")
+      .attr("width", width)
+      .attr("height", height - 50)
+      .selectAll("g")
+      .data(statKeys)
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) {
+         if (i === 0) {
+            dataL = d.length + offset
+            return "translate(0,0)"
+        } else {
+         var newdataL = dataL
+         dataL +=  d.length + offset
+         return `translate(${newdataL},0)`
+        }
+  })
+
+  legend.append("rect")
+      .attr("x", legendPaddingX)
+      .attr("y", svgHeight - legendPaddingY)
+      .attr("width", legendColorBoxSize)
+      .attr("height", legendColorBoxSize)
+      .style("fill", function (d, i) {
+      return colors(i)
+  })
+
+  legend.append("text")
+      .attr("x", legendPaddingX + 20)
+      .attr("y", svgHeight - legendPaddingY + 10)
+  .attr("dy", ".1em")
+  .text(function (d, i) {
+      return d
+  })
+  .attr("class", "textselected")
+  .style("text-anchor", "start")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", 10);
+
+
 };
 
 
@@ -106,6 +160,6 @@ function plotNbaGroupedBar(error, data, graphDivId) {
 
 
 /* SCORING GRAPH */
-d3.json('../static/nba-grouped-bar-data.json', function(error, data) {
-  plotNbaGroupedBar(error, data, '#scoring_graph');
+d3.json("../static/nba-grouped-bar-data.json", function(error, data) {
+  plotNbaGroupedBar(error, data, "#scoring_graph");
 });
